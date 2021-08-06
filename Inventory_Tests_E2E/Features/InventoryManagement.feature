834d9102516b1a8ceec71cf726bf1c805ac371df
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-08-06 08:35:27 +0430

    Add Inventory Management Feature And Defining A New Inventory Scenario

---
+++ Inventory_Tests_E2E/Features/InventoryManagement.feature
@@ -0,0 +1,13 @@
+﻿Feature: Inventory Management
+	As A Warehouse Keeper
+	I Want To Keep Track About Products Stock
+	So That I Always Have Accurate Info Of Inventory
+
+@api
+Scenario: Defining A New Inventory
+	Given I Want To Define The Following Inventory
+	| Product | UnitPrice |
+	| Asus    | 1100      |
+	When I Try To Define This Inventory
+	Then The Inventory Should Be Created
+	And It Should Be Empty
---
+++ Inventory_Tests_E2E/Steps/InventoryManagementSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Inventory_Tests_E2E.Steps
+{
+    [Binding]
+    public class InventoryManagementSteps
+    {
+        [Given(@"I Want To Define The Following Inventory")]
+        public void GivenIWantToDefineTheFollowingInventory(Table table)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [When(@"I Try To Define This Inventory")]
+        public void WhenITryToDefineThisInventory()
+        {
+        }
+        
+        [Then(@"The Inventory Should Be Created")]
+        public void ThenTheInventoryShouldBeCreated()
+        {
+        }
+        
+        [Then(@"It Should Be Empty")]
+        public void ThenItShouldBeEmpty()
+        {
+        }
+    }
+}
