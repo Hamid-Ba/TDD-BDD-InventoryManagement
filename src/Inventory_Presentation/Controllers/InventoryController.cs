83cf66296cc82caf146e678bf4ad3b54b0b50a0d
hamid balalzadeh <balalzadehhamid79@gmail.com>
2021-08-07 16:16:31 +0430

    Add Inventory Presentation Unit Tests

---
+++ src/Inventory_Presentation/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Inventory_Application_Contract.InventoryAgg;
+
+namespace Inventory_Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryApplication _application;
+
+        public InventoryController(IInventoryApplication application) => _application = application;
+
+        [HttpPost]
+        public bool Create(InventoryDefineVM command)
+        {
+            try
+            {
+                return _application.Define(command);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/Inventory_Presentation_Tests_Unit/Tests/InventoryControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Inventory_Application_Contract.InventoryAgg;
+using Inventory_Presentation.Controllers;
+using NSubstitute;
+using Xunit;
+
+namespace Inventory_Presentation_Tests_Unit.Tests
+{
+    public class InventoryControllerTests
+    {
+        private readonly InventoryController _controller;
+        private readonly IInventoryApplication _application;
+
+        public InventoryControllerTests()
+        {
+            _application = Substitute.For<IInventoryApplication>();
+            _controller = new InventoryController(_application);
+        }
+
+        [Fact]
+        public void Should_Create_Method_Work_Properly()
+        {
+            //arrange
+            var command = Utils.Utils.SomeCreateCode();
+
+            //act
+            _controller.Create(command);
+
+            //assertion
+            _application.ReceivedWithAnyArgs().Define(Arg.Any<InventoryDefineVM>());
+        }
+
+    }
+}
